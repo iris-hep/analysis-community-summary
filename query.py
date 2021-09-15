2a2960efb06cb1747634ea60edeb963310428c4a
Matthew Feickert <matthew.feickert@cern.ch>
2021-09-14 22:13:56 -0500

    Add all repos to query list

--- query.py
+++ query.py
@@ -2,29 +2,62 @@ import json
 from urllib import request
 
 
-def getpage(url, n=1):  # starts with 1
+def get_page(url, n=1):
+    # starts with 1
     data = json.loads(request.urlopen(url + "&page=" + str(n)).read())
     if len(data) == 0:
         return data
     else:
-        return data + getpage(url, n + 1)
+        return data + get_page(url, n + 1)
 
 
 if __name__ == "__main__":
-    awkward_0 = set(
-        [
-            x["login"]
-            for x in getpage(
-                "https://api.github.com/repos/scikit-hep/awkward-0.x/stargazers?per_page=100"
-            )
-        ]
-    )
-    awkward_1 = set(
-        [
-            x["login"]
-            for x in getpage(
-                "https://api.github.com/repos/scikit-hep/awkward-1.0/stargazers?per_page=100"
-            )
-        ]
-    )
-    print(f"Intersection of awkard0 and awkward1 stars: {len(awkward_0 | awkward_1)}")
+    repo_names = [
+        "GooFit/AmpGen",
+        "GooFit/GooFit",
+        "alexander-held/cabinetry",
+        "diana-hep/excursion",
+        "diana-hep/madminer",
+        "func_adl_servicex",
+        "func_adl_uproot",
+        "func_adl_xAOD",
+        "gordonwatts/hep_tables",
+        "iris-hep/adl-benchmarks-index",
+        "iris-hep/func_adl",
+        "iris-hep/qastle",
+        "reanahub/reana",
+        "root-project/root",
+        "scailfin/madminer-workflow",
+        "scikit-hep/awkward-0.x",
+        "scikit-hep/awkward-1.0",
+        "scikit-hep/boost-histogram",
+        "scikit-hep/cookie",
+        "scikit-hep/decaylanguage",
+        "scikit-hep/fastjet",
+        "scikit-hep/hist",
+        "scikit-hep/mplhep",
+        "scikit-hep/particle",
+        "scikit-hep/pyhf",
+        "scikit-hep/uhi",
+        "scikit-hep/uproot3",
+        "scikit-hep/uproot4",
+        "scikit-hep/vector",
+    ]
+
+    data = {
+        repo_name: {
+            "stars": get_page(
+                f"https://api.github.com/repos/{repo_name}/stargazers?per_page=100"
+            ),
+            "watchers": get_page(
+                f"https://api.github.com/repos/{repo_name}/subscribers?per_page=100"
+            ),
+            "forks": get_page(
+                f"https://api.github.com/repos/{repo_name}/forks?per_page=100"
+            ),
+        }
+        for repo_name in repo_names
+    }
+
+    with open("repo_data.json", "w") as write_file:
+        write_file.write(json.dumps(data))

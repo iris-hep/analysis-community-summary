7c988541f8de8e78d45b0e30124f431451b67072
Matthew Feickert <matthew.feickert@cern.ch>
2021-09-15 09:38:53 -0500

    Add logic for reach

--- summary.py
+++ summary.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 if __name__ == "__main__":
     with open(Path().cwd().joinpath("repo_data.json")) as read_file:
-        repo_data = json.load(read_file)
+        data = json.load(read_file)
 
     summary_table = (
         f"{'GitHub repository':81} | {'Stars':5} | {'Watch':5} | {'Forks':5}\n"
@@ -14,32 +14,42 @@ if __name__ == "__main__":
     all_stars = 0
     all_watchers = 0
     all_forks = 0
-    for repo in repo_data:
+    for repo in data:
         if repo != "root-project/root":
-            stars = repo_data[repo]["stars"]
-            watchers = repo_data[repo]["watchers"]
-            forks = repo_data[repo]["forks"]
-            repo_markdown_link = f"[{repo}](https://github.com/{repo})"
-            summary_table += (
-                f"{repo_markdown_link:81} | {stars:5} | {watchers:5} | {forks:5}\n"
-            )
+            repo_data = data[repo]
 
-            all_stars += stars
-            all_watchers += watchers
-            all_forks += forks
+            stargazer_ids = set(repo_data["stargazer_ids"])
+            watcher_ids = set(repo_data["watcher_ids"])
+            fork_owner_ids = set(repo_data["fork_owner_ids"])
+            reach = stargazer_ids | watcher_ids | fork_owner_ids
+
+            star_count = repo_data["star_count"]
+            watcher_count = repo_data["watcher_count"]
+            fork_count = repo_data["fork_count"]
+            repo_markdown_link = f"[{repo}](https://github.com/{repo})"
+            summary_table += f"{repo_markdown_link:81} | {star_count:5} | {watcher_count:5} | {fork_count:5}\n"
+
+            all_stars += star_count
+            all_watchers += watcher_count
+            all_forks += fork_count
 
     summary_table += f"{'-' * 81}-|-{'-' * 5}-|-{'-' * 5}-|-{'-' * 5}\n"
     summary_table += f"{'All IRIS-HEP Analysis Systems':81} | {all_stars:5} | {all_watchers:5} | {all_forks:5}\n"
 
-    for repo in repo_data:
+    for repo in data:
         if repo == "root-project/root":
-            stars = repo_data[repo]["stars"]
-            watchers = repo_data[repo]["watchers"]
-            forks = repo_data[repo]["forks"]
+            repo_data = data[repo]
+
+            stargazer_ids = set(repo_data["stargazer_ids"])
+            watcher_ids = set(repo_data["watcher_ids"])
+            fork_owner_ids = set(repo_data["fork_owner_ids"])
+            reach = stargazer_ids | watcher_ids | fork_owner_ids
+
+            star_count = repo_data["star_count"]
+            watcher_count = repo_data["watcher_count"]
+            fork_count = repo_data["fork_count"]
             repo_markdown_link = f"[{repo}](https://github.com/{repo})"
-            summary_table += (
-                f"{repo_markdown_link:81} | {stars:5} | {watchers:5} | {forks:5}\n"
-            )
+            summary_table += f"{repo_markdown_link:81} | {star_count:5} | {watcher_count:5} | {fork_count:5}\n"
 
     with open("summary.md", "w") as write_file:
         write_file.write("\n## Summary Table\n\n")

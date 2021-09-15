c445bda51c900aeebfd710ae47b8611dadac6781
Matthew Feickert <matthew.feickert@cern.ch>
2021-09-15 01:33:35 -0500

    Focus on table

--- summary.py
+++ summary.py
@@ -6,9 +6,10 @@ if __name__ == "__main__":
         repo_data = json.load(read_file)
 
     summary_table = (
-        f"{'GitHub repository':30} | {'stars':5} | {'watch':5} | {'forks':5}\n"
+        f"{'GitHub repository':81} | {'Stars':5} | {'Watch':5} | {'Forks':5}\n"
     )
-    summary_table += f"{'-' * 30}-|-{'-' * 5}-|-{'-' * 5}-|-{'-' * 5}\n"
+    # Ending header with colon right aligns
+    summary_table += f"{'-' * 81}-|-{'-' * 5}:|-{'-' * 5}:|-{'-' * 4}:\n"
 
     all_stars = 0
     all_watchers = 0
@@ -18,25 +19,30 @@ if __name__ == "__main__":
             stars = repo_data[repo]["stars"]
             watchers = repo_data[repo]["watchers"]
             forks = repo_data[repo]["forks"]
-            # reach = stars | watchers | forks
-            summary_table += f"{repo:30} | {stars:5} | {watchers:5} | {forks:5}\n"
+            repo_markdown_link = f"[{repo}](https://github.com/{repo})"
+            summary_table += (
+                f"{repo_markdown_link:81} | {stars:5} | {watchers:5} | {forks:5}\n"
+            )
 
             all_stars += stars
             all_watchers += watchers
             all_forks += forks
 
-    summary_table += f"{'-' * 30}-|-{'-' * 5}-|-{'-' * 5}-|-{'-' * 5}\n"
-    summary_table += f"{'All IRIS-HEP Analysis Systems':30} | {all_stars:5} | {all_watchers:5} | {all_forks:5}\n"
+    summary_table += f"{'-' * 81}-|-{'-' * 5}-|-{'-' * 5}-|-{'-' * 5}\n"
+    summary_table += f"{'All IRIS-HEP Analysis Systems':81} | {all_stars:5} | {all_watchers:5} | {all_forks:5}\n"
 
     for repo in repo_data:
         if repo == "root-project/root":
             stars = repo_data[repo]["stars"]
             watchers = repo_data[repo]["watchers"]
             forks = repo_data[repo]["forks"]
-            # reach = stars | watchers | forks
-            summary_table += f"{repo:30} | {stars:5} | {watchers:5} | {forks:5}\n"
+            repo_markdown_link = f"[{repo}](https://github.com/{repo})"
+            summary_table += (
+                f"{repo_markdown_link:81} | {stars:5} | {watchers:5} | {forks:5}\n"
+            )
 
     with open("summary.md", "w") as write_file:
+        write_file.write("\n## Summary Table\n\n")
         write_file.write(summary_table)
 
     print(summary_table)

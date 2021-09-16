f88b4a3dbb2bd1e917c2815db70a62d110a5acb2
Matthew Feickert <matthew.feickert@cern.ch>
2021-09-15 23:29:28 -0500

    feat: Output summary as CSV and add tidy data time series deploy (#6)
    * Add export of summary data frame as a CSV
    * Add download button for summary CSV to README
    * Add script to generate a "tidy data" time series by appending the summary CSV to previous dates CSVs
       - If the summary CSV has the same date as the previous CSV, update the current date's entries
       - c.f. https://vita.had.co.nz/papers/tidy-data.html for Tidy Data
    * Add a time series deploy stage to the CI to deploy the time series CSV to a 'time-series' branch
       - Unlike the gh-pages branch the time-series branch is not orphaned and keeps commits to aid in Git Scraping
       - c.f. https://simonwillison.net/2020/Oct/9/git-scraping/ for Git Scraping

---
+++ time_series.py
@@ -0,0 +1,40 @@
+import argparse
+
+import pandas as pd
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Update the time series CSV.")
+    parser.add_argument(
+        "in_csv",
+        metavar="IN_CSV",
+        type=str,
+        help="Path of input CSV",
+    )
+    parser.add_argument(
+        "--join-csv",
+        dest="join_csv",
+        type=str,
+        default="summary.csv",
+        help="Path of CSV to join or update IN_CSV",
+    )
+    parser.add_argument(
+        "--out-csv",
+        dest="out_csv",
+        type=str,
+        default="summary-time-series.csv",
+        help="Path of output CSV",
+    )
+    args = parser.parse_args()
+
+    update_df = pd.read_csv(args.in_csv)
+    current_df = pd.read_csv(args.join_csv)
+
+    if current_df.date.isin(update_df.date)[0]:
+        # Overwrite dataframe with current values
+        selection = update_df["date"] == current_df["date"][0]
+        update_df[selection] = current_df
+    else:
+        # Append current values
+        update_df = pd.concat([update_df, current_df])
+
+    update_df.to_csv(args.out_csv, index=False)

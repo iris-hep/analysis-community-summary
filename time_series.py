68730801458c3c528f487dc3a60adbfefc25dc78
Matthew Feickert <matthew.feickert@cern.ch>
2021-09-15 22:34:05 -0500

    Add time series

---
+++ time_series.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import pandas as pd
+
+if __name__ == "__main__":
+    url = "https://raw.githubusercontent.com/iris-hep/analysis-community-summary/gh-pages/summary_time_series.csv"
+    update_df = pd.read_csv(url)
+    current_df = pd.read_csv(Path().cwd().joinpath("summary.csv"))
+
+    if current_df.date.isin(update_df.date)[0]:
+        # Overwrite dataframe with current values
+        selection = update_df["date"] == current_df["date"][0]
+        update_df[selection] = current_df
+    else:
+        # Append current values
+        update_df = pd.concat([update_df, current_df])
+
+    update_df.to_csv("summary_time_series.csv", index=False)
